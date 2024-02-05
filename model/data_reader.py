6e93ed46dfa00756b6e90efadd9d2f967b3d348b
Carlotta <50404166+carlotta-mah@users.noreply.github.com>
2024-02-05 11:14:00 +0200

    add interface for data reader

---
+++ model/data_reader.py
@@ -0,0 +1,11 @@
+class DataReader:
+    def __init__(self, path):
+        self.path = path
+
+    # reads all sequences if file is none
+    def get_sequences(self, file=None):
+
+        pass
+
+if __name__ == "__main__":
+    data_reader = DataReader("Development/Keystrokes/files");
