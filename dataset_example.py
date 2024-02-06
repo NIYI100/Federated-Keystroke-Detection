c5510a81b33b40cf0c99a416d4aed34ed1b6daf7
jonaszilke <jonas.zilke@gmail.com>
2024-02-06 11:12:46 +0200

    save dataset in dataset_example

--- dataset_example.py
+++ dataset_example.py
@@ -1,11 +1,17 @@
+import random
 import torch
 from model.dataset import KeystrokeDataset
-
-keystrokes_series = []
-
-t = torch.rand(10, 3)
-labels = torch.tensor([1])
+import os
 
 dataset = KeystrokeDataset()
-dataset.append_item(t, labels)
 
+for i in range(10000):
+    t = torch.rand(10, 3)
+    labels = torch.tensor([random.randint(0, 1)])
+    dataset.append_item(t, labels)
+
+prefix = "./dataset"
+if not os.path.exists(prefix):
+    os.makedirs(prefix)
+path = f"{prefix}/test.pt"
+torch.save(dataset, path)
