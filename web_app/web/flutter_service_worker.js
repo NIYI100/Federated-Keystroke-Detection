27f55e63632cdfc189ac04ccee6015b076e1fb59
svena <sven.ambrosius100@gmail.com>
2024-03-08 16:23:18 +0200

    Added different paths for classification

--- web_app/web/flutter_service_worker.js
+++ web_app/web/flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7d4299d25090fc939112506b49823575",
-"/": "7d4299d25090fc939112506b49823575",
-"main.dart.js": "16d7f9cf36f01625264921d2ac4fd1c8",
+"index.html": "c14ff5803572628f8aa672c9f449d695",
+"/": "c14ff5803572628f8aa672c9f449d695",
+"main.dart.js": "10654aeb3c691f59e305971f4bf70b31",
 "manifest.json": "2eea505589fa67dfb92a3f958145c070",
 "version.json": "c53eb8b395839657972194b1cde4880a"};
 // The application shell files that are downloaded before a service worker can
