2d6c1162f96816f66f03c74f751060ef1e03d9aa
Robert Jaschek <robert.jaschek@t-online.de>
2024-03-10 16:32:11 +0200

    setup apache via config file

--- oakestra/docker/run.sh
+++ oakestra/docker/run.sh
@@ -3,7 +3,6 @@
 #PYTHONPATH=/app python3 model/train.py &
 #PYTHONUNBUFFERED='True' python3 /backend/main.py &
 sed -i -e "s/http:\/\/localhost:8080/http:\/\/keystrokedetector/g" /usr/local/apache2/htdocs/main.dart.js
-sed -i -e "s/Listen 80/Listen 8000/g" /usr/local/apache2/conf/httpd.conf
 cd /backend
 /usr/local/bin/uwsgi --pythonpath /ai_model --ini /backend/wsgi.ini &
 nginx &
