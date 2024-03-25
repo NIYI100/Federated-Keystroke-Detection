2b21bf19e925734df17bc6d8feb10c19462af644
Robert Jaschek <robert.jaschek@t-online.de>
2024-03-25 11:08:11 +0200

    adjust for training API

--- oakestra/docker/run.sh
+++ oakestra/docker/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#cd /app
-#PYTHONPATH=/app python3 model/train.py &
-#PYTHONUNBUFFERED='True' python3 /backend/main.py &
 sed -i -e "s/http:\/\/localhost:8080/http:\/\/keystrokedetector/g" /usr/local/apache2/htdocs/main.dart.js
 cd /backend
 /usr/local/bin/uwsgi --pythonpath /ai_model --ini /backend/wsgi.ini &
