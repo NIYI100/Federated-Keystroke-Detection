e803a2014aa5bb81aafd6beb38f5003a1cbd4ebd
Robert Jaschek <robert.jaschek@t-online.de>
2024-03-25 11:07:54 +0200

    implemented training interface

--- web_app/backend/main.py
+++ web_app/backend/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from keystroke_generator.keystroke_generator import KeystrokeGenerator
+from utils import store_new_keystroke_series
 from model.run_network import classify_sentence
+import model.train as train
 import json
 import pandas as pd
 import torch
@@ -11,9 +13,17 @@ app = Flask(__name__)
 
 @app.route('/training', methods=['POST'])
 def training():
+    num_epochs = 1
     query_json = request.json
     print(json.dumps(query_json, indent=1))
-    # TODO: train local model
+    file_location = "/ai_model/dataset/train/train.pt"
+    len_new_train_set = store_new_keystroke_series(query_json, file_location)
+    if (len_new_train_set % 200) == 0:
+        print("train with new dataset")
+        train.setup()
+        for epoch_idx in range(num_epochs):
+            loss_epoch = train.train_epoch(epoch_idx)
+            train.validate(epoch_idx, loss_epoch)
     resp = flask.Response(response=query_json, status=200)
     return resp
 
@@ -22,9 +32,9 @@ def training():
 def classification():
     query_json = request.json
     query_json = [query_json[i] for i in query_json]
-    #print(json.dumps(query_json, indent=1))
+    print(json.dumps(query_json, indent=1))
     dask_df = pd.DataFrame(query_json)
-    #print(dask_df)
+    # print(dask_df)
     dask_df['SEQUENCE_START_TIME'] = dask_df.groupby(['testSectionId'])['pressTime'].transform('min')
     dask_df['PRESS_TIME_RELATIVE'] = dask_df['pressTime'] - dask_df['SEQUENCE_START_TIME']
     dask_df['data'] = dask_df[['PRESS_TIME_RELATIVE', 'duration', 'jsKeyCode']].values.tolist()
