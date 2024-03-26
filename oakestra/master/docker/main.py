9d603c84e4a2ddd60153d4c509f838efc157b5b0
Robert Jaschek <robert.jaschek@t-online.de>
2024-03-26 12:41:31 +0200

    setup master node

--- oakestra/master/docker/main.py
+++ oakestra/master/docker/main.py
@@ -14,23 +14,28 @@ def main():
     session = requests.Session()
     session.trust_env = False
     model_weights_list = []
-    node_list = ["localhost"]
+    node_list = ["64.0.0.6"]
     while True:
-        for node in node_list:
-            x = session.get(f'http://{node}/getweights')
-            curr_model_weights = pickle.loads(x.content)
-            print(curr_model_weights)
-            model_weights_list.append(curr_model_weights)
-        avg_model_weight = pickle.dumps(calculate_averages(model_weights_list))
-        model_weights_list = []
-        for node in node_list:
-            res = session.post(url=f'http://{node}/setweights',
-                                data=avg_model_weight,
-                                headers={'Content-Type': 'application/octet-stream'}
-                                )
-            print(res)
-        # wait 5 minutes
-        time.sleep(300)
+        try:
+            for node in node_list:
+                x = session.get(f'http://{node}/getweights')
+                curr_model_weights = pickle.loads(x.content)
+                print(x)
+                model_weights_list.append(curr_model_weights)
+            avg_model_weight = pickle.dumps(calculate_averages(model_weights_list))
+            model_weights_list = []
+            for node in node_list:
+                res = session.post(url=f'http://{node}/setweights',
+                                    data=avg_model_weight,
+                                    headers={'Content-Type': 'application/octet-stream'}
+                                    )
+                print(res)
+            # wait 5 minutes
+            time.sleep(60)
+        except Exception as e:
+            print(e)
+            # wait 5 minutes
+            time.sleep(60)
 
 
 if __name__ == '__main__':
